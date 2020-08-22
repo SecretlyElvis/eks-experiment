f79d9ac396734bf12bd274eb03838ed5f6b49ec4
secretlyelvis <auldd@neuralessence.com>
2020-08-23 04:13:44 +1200

    Minor fine-tuning tweaks

--- k8s/k8s.tf
+++ k8s/k8s.tf
@@ -1,28 +1,28 @@
-# data "terraform_remote_state" "remote" {
-#  backend = "s3"
-#  config = {
-#    bucket = "example-s3-terraform"
-#    key    = "aws-provider.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+ data "terraform_remote_state" "remote" {
+  backend = "s3"
+  config = {
+    bucket = "neuralessence-terraform-state"
+    key    = "eks-experiment.tfstate"
+    region = "ap-southeast-2"
+  }
+}
 
-#resource "kubernetes_persistent_volume" "example" {
-#  metadata {
-#    name = "example-efs-pv"
-#  }
-#  spec {
-#    storage_class_name = "efs-sc"
-#    persistent_volume_reclaim_policy = "Retain"
-#    capacity = {
-#      storage = "2Gi"
-#    }
-#    access_modes = ["ReadWriteMany"]
-#    persistent_volume_source {
-#      nfs {
-#        path = "/"
-#        server = data.terraform_remote_state.remote.outputs.efs_example_fsid
-#      }
-#    }
-#  }
-#}
+resource "kubernetes_persistent_volume" "example" {
+  metadata {
+    name = "jenkins-efs-pv"
+  }
+  spec {
+    storage_class_name = "efs-sc"
+    persistent_volume_reclaim_policy = "Retain"
+    capacity = {
+      storage = "2Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    persistent_volume_source {
+      nfs {
+        path = "/"
+        server = data.terraform_remote_state.remote.outputs.jenkins-fs_fsid
+      }
+    }
+  }
+}
